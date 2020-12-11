8f86e1a1cc538577446473e14d6595e882458c5c
Nathan B Wang <nbwang22@gmail.com>
2020-12-11 12:27:06 -0500

    Converted keyence_file_managment to kfm module

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Keyence File Management',
+    description='Helps manage files from Keyence for the Galloway lab',
+        url='https://github.com/GallowayLabMIT/keyence_file_management',
+        author='Nathan B. Wang',
+        author_email='nbwang22@gmail.com',
+        license='MIT',
+        packages=['keyence_file_management'],
+        install_requires=[],
+        zip_safe=True,
+        entry_points={
+        "console_scripts": [
+            "kfm=keyence_file_management:entrypoint"
+            ]
+        }
+)
